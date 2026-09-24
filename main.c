81da5d392775e3a07b6c5b85c8f51a09ff48364a
Pedro Lavado <p.lavado.2023@alumnos.urjc.es>
2026-09-24 10:27:53 +0200

    version1

--- main.c
+++ main.c
@@ -1,10 +1,33 @@
 #include <stdio.h>
 
-//dddd
-int main() {
-	printf("Hola MundoX\n");
-	return 0;
+int numentrada;
+char letraentrada;
+char letradesplazada;
 
+void leerdata(int desplazamiento, char letra) {
+	
+	printf("Intoduzca la letra a desplazar: \n");
+	scanf("%c", &letra);
+	printf("Intoduzca el desplazamiento: \n");
+	scanf("%d", &desplazamiento);
+}
+
+
+char desplazarletra(int desplazamiento, char letra) {
+	letradesplazada = letraentrada + desplazamiento;
+
+	return letradesplazada;
+}
+
+
+
+int main() {
+	
+	leerdata(numentrada, letraentrada);
+	desplazarletra(numentrada,letraentrada);
+	printf("%c",letradesplazada);
+
+	return 0;
 	
 }
 
